7a566fa7ae6bdc5346ae86ea2e4a67e99af20c31
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-03-15 23:37:22 +0000

    header and footer added by EJS

--- BlogPost/app.js
+++ BlogPost/app.js
@@ -16,9 +16,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-
 app.get('/',(req,res)=>{
-  res.render('home')
+    
+   res.render('home',{startingContent:homeStartingContent});
 });
 
 
